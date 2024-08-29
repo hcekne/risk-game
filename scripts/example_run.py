18ed7eba610c1f34bf924800a1b6953f27acc585
Hans Christian Ekne <hc.ekne@gmail.com>
2024-08-29 09:17:30 +0100

    updated example_run.py

--- scripts/example_run.py
+++ scripts/example_run.py
@@ -26,40 +26,40 @@ experiment.run_experiment()
 
 
 
-#################
-# Example 2
+# #################
+# # Example 2
 
-from risk_game.rules import Rules
-from risk_game.game_config import GameConfig
-from risk_game.game_master import GameMaster as gm
-from risk_game.llm_clients import llm_client
+# from risk_game.rules import Rules
+# from risk_game.game_config import GameConfig
+# from risk_game.game_master import GameMaster as gm
+# from risk_game.llm_clients import llm_client
 
-# Correct: Creating an instance of GameConfig
-config = GameConfig(progressive=True, capitals=False, max_rounds=2)
+# # Correct: Creating an instance of GameConfig
+# config = GameConfig(progressive=True, capitals=False, max_rounds=2)
 
-# Correct: Creating an instance of Rules
-rules = Rules(config)
+# # Correct: Creating an instance of Rules
+# rules = Rules(config)
 
-rules
+# rules
 
-g1 = gm(rules)
+# g1 = gm(rules)
 
-g1.add_player(name="llama3.1_70b_AWS", 
-                        llm_client=llm_client.create_llm_client("Bedrock", 2))
+# g1.add_player(name="llama3.1_70b_AWS", 
+#                         llm_client=llm_client.create_llm_client("Bedrock", 2))
 
-g1.players[0].send_message("Can you help me play risk? I'm new to this game.")
+# g1.players[0].send_message("Can you help me play risk? I'm new to this game.")
 
-# g1.add_player(name="Test Player 1", 
-#                         llm_client=llm_client.create_llm_client("Groq", 2))
-# g1.add_player(name="Test Player 2", 
-#             llm_client=llm_client.create_llm_client("OpenAI", 2))
-# g1.add_player(name="Test Player 3", 
-#             llm_client=llm_client.create_llm_client("OpenAI", 2))
+# # g1.add_player(name="Test Player 1", 
+# #                         llm_client=llm_client.create_llm_client("Groq", 2))
+# # g1.add_player(name="Test Player 2", 
+# #             llm_client=llm_client.create_llm_client("OpenAI", 2))
+# # g1.add_player(name="Test Player 3", 
+# #             llm_client=llm_client.create_llm_client("OpenAI", 2))
 
-print(g1.players[0])
+# print(g1.players[0])
 
-g1
+# g1
 
-g1.play_game()
+# g1.play_game()
 
 
